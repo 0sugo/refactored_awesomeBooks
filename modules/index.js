6b43db491cc1730cb9c302e96e4816ec2d2c702a
0sugo <josugo26@gmail.com>
2023-03-28 12:19:04 +0300

    Compile index.js

---
+++ modules/index.js
@@ -0,0 +1,47 @@
+import * as attach from './app.js';
+import Books from './books.js';
+
+import { DateTime } from './luxon.js';
+
+const now = DateTime.now();
+const date = now.toLocaleString(DateTime.DATETIME_FULL);
+document.getElementById('date').textContent = date;
+
+
+function getnew (){
+    const bookItem2 = new Books();
+    bookItem2.retrieve();
+  
+  }
+  
+  const addButton = document.getElementById('add');
+  addButton.addEventListener('click', () => {
+  console.log("clicked")
+    const bookItem = new Books();
+    bookItem.update().retrieve();
+  });
+  
+  const lister = document.querySelector('#lister');
+  const contacter = document.querySelector('#contacter');
+  const adder = document.querySelector('#unique-border');
+  
+  const list = document.querySelector('.list-section');
+  const add = document.querySelector('.add');
+  const contact = document.querySelector('.contact');
+  
+  lister.addEventListener('click', () => {
+    list.style.display = 'block';
+    add.style.display = 'none';
+    contact.style.display = 'none';
+    getnew();
+  });
+  adder.addEventListener('click', () => {
+    add.style.display = 'block';
+    list.style.display = 'none';
+    contact.style.display = 'none';
+  });
+  contacter.addEventListener('click', () => {
+    contact.style.display = 'flex';
+    add.style.display = 'none';
+    list.style.display = 'none';
+  });
